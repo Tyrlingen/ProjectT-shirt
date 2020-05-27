7ee8ddc162ef31a95a026a42976c40e39659f498
Sebastian <sebastian_tyrling@hotmail.com>
2020-05-27 10:34:42 +0200

    Deleted som files and initiated React app aswell as word api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentText: []
+    };
+  }
+  wordGetter(){
+    fetch("https://random-word-api.herokuapp.com/word?number=10")
+    .then((response)=>{
+      return response.json();
+    })
+    .then((data)=>{
+      this.setState({currentText: data});
+    })
+    
+  }
+  render(){
+    console.log(this.state.currentText); 
+    return(<main>
+        <p>"blabla"</p>
+        <button onClick={this.wordGetter.bind(this)}>Klick me!</button>
+    </main>)
+  }
 }
+    
 
 export default App;
