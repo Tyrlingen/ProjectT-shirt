6e28947687cd35db5ae3f35eb93a3059a7e8a667
Felicia <felicia.lindback@outlook.com>
2020-05-29 11:09:58 +0200

    Combined handleClick() and handleKeyPress to one function

--- src/App.js
+++ src/App.js
@@ -1,48 +1,64 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: " "
+      text: " ",
     };
   }
-  handleKeyPress(event){
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const inputField = document.querySelector("#word-count");
+    const number = inputField.value;
+    fetch(`https://random-word-api.herokuapp.com/word?number=${number}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const text = data.join(" ");
+        this.setState({ text: text });
+      });
+  }
+  handleKeyPress(event) {
     if (event.which === 13) {
       const inputField = document.querySelector("#word-count");
       const number = inputField.value;
       this.wordGetter(number);
+    }
   }
-  }
-  handleClick(){
+  handleClick() {
     const inputField = document.querySelector("#word-count");
     const number = inputField.value;
     this.wordGetter(number);
   }
-  wordGetter(number){
-
+  wordGetter(number) {
     fetch(`https://random-word-api.herokuapp.com/word?number=${number}`)
-    .then((response)=>{
-      return response.json();
-    })
-    .then((data)=>{
-      const text = data.join(" ")
-      this.setState({text: text});
-    })
-    }
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const text = data.join(" ");
+        this.setState({ text: text });
+      });
+  }
 
-  render(){ 
-    return(<main>
+  render() {
+    return (
+      <main>
+        <form id="generate-words" onSubmit={this.handleSubmit.bind(this)}>
           <label for="word-count">Word count:</label>
-          <input id="word-count" type="number"  onKeyPress = {this.handleKeyPress.bind(this)}></input>
-        <button onClick={this.handleClick.bind(this)}>Klick me!</button>
+          <input id="word-count" type="number" />
+          <input type="submit" />
+        </form>
         <div className="tshirt-container">
           <p>{this.state.text}</p>
         </div>
-    </main>)
+      </main>
+    );
   }
 }
-    
 
 export default App;
