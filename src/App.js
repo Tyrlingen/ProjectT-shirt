f1319ff421471af4df847002a82e3112a633f7a6
Felicia <felicia.lindback@outlook.com>
2020-05-30 12:11:49 +0200

    Can know change shirt color by clicking buttons.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,29 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: " ",
       savedShirts: [],
-      currentShirtColor: "",
-      currentTextColor: "",
-      currentShirtFont: "",
+      text: " ",
+      textAlign: "right",
+      shirtColor: "",
+      textColor: "",
+      shirtFont: "",
     };
   }
 
+  changeShirtColor(color) {
+    this.setState({ shirtColor: color });
+  }
+
+  getClasses() {
+    var text = "text-align--";
+    var shirt = "shirt-color--";
+
+    var textAlign = text + this.state.textAlign;
+    var shirtColor = shirt + this.state.shirtColor;
+    var textColor = text;
+
+    return [textAlign, shirtColor].join(" ");
+  }
   handleSubmit(event) {
     event.preventDefault();
     const inputField = document.querySelector("#word-count");
@@ -28,6 +43,7 @@ class App extends Component {
   }
 
   render() {
+    const shirtClasses = this.getClasses();
     return (
       <main>
         <form id="generate-words" onSubmit={this.handleSubmit.bind(this)}>
@@ -35,7 +51,33 @@ class App extends Component {
           <input id="word-count" type="number" />
           <input type="submit" />
         </form>
-        <div className="tshirt-container text--center">
+        <div className="shirt-color-options">
+          <button
+            className="color-btn color-btn--white"
+            onClick={() => this.changeShirtColor("white")}
+          ></button>
+          <button
+            className="color-btn color-btn--black "
+            onClick={() => this.changeShirtColor("black")}
+          ></button>
+          <button
+            className="color-btn color-btn--green"
+            onClick={() => this.changeShirtColor("green")}
+          ></button>
+          <button
+            className="color-btn color-btn--blue"
+            onClick={() => this.changeShirtColor("blue")}
+          ></button>
+          <button
+            className="color-btn color-btn--red"
+            onClick={() => this.changeShirtColor("red")}
+          ></button>
+          <button
+            className="color-btn color-btn--yellow"
+            onClick={() => this.changeShirtColor("yellow")}
+          ></button>
+        </div>
+        <div className={`tshirt-container shirt-color--white ${shirtClasses}`}>
           <div className="tshirt">
             <p>{this.state.text}</p>
           </div>
