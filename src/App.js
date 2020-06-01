e440ee4b242743ed97233c57b6b21de457e8d062
Felicia <felicia.lindback@outlook.com>
2020-06-01 11:41:22 +0200

    Changed App.js to router component and created new components

--- src/App.js
+++ src/App.js
@@ -1,51 +1,17 @@
 import React, { Component } from "react";
+import Generator from "./Generator";
+import Saved from "./Saved";
 import "./App.scss";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  NavLink,
+} from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      savedShirts: [],
-      text: " ",
-      textAlign: "left",
-      shirtColor: "",
-      textColor: "",
-      shirtFont: "",
-    };
-
-    this.colorList = ["white", "black", "green", "blue", "red", "yellow"];
-  }
-
-  componentDidMount() {
-    this.getShirts();
-  }
-
-  getShirts() {
-    const myStorage = window.localStorage;
-    const shirts = myStorage.getItem("saved-shirts");
-    if (shirts && shirts.length) {
-      this.setState({ savedShirts: JSON.parse(shirts) });
-    }
-  }
-
-  saveShirt() {
-    const shirt = {
-      text: this.state.text,
-      textAlign: this.state.textAlign,
-      shirtColor: this.state.shirtColor,
-      textColor: this.state.textColor,
-      shirtFont: this.state.shirtFont,
-    };
-    const newSavedShirts = [...this.state.savedShirts, shirt];
-    this.setState({ savedShirts: newSavedShirts });
-    const myStorage = window.localStorage;
-    myStorage.setItem("saved-shirts", JSON.stringify(newSavedShirts));
-  }
-
-  changeColorOf(thing, color) {
-    thing == "shirt"
-      ? this.setState({ shirtColor: color })
-      : this.setState({ textColor: color });
   }
 
   getClasses(textAlign, shirtColor, textColor) {
@@ -59,104 +25,21 @@ class App extends Component {
 
     return [newTextAlign, newShirtColor, newTextColor].join(" ");
   }
-  handleSubmit(event) {
-    event.preventDefault();
-    const inputField = document.querySelector("#word-count");
-    const number = inputField.value;
-    fetch(`https://random-word-api.herokuapp.com/word?number=${number}`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const text = data.join(" ");
-        this.setState({ text: text });
-      });
-  }
-
-  changeTextAlign(event) {
-    console.log(event.target.value);
-    this.setState({ textAlign: event.target.value });
-  }
-
-  renderColorButton(thing, color) {
-    return (
-      <button
-        className={`color-btn color-btn--${color}`}
-        onClick={() => this.changeColorOf(thing, color)}
-      ></button>
-    );
-  }
 
   render() {
-    const shirtClasses = this.getClasses(
-      this.state.textAlign,
-      this.state.shirtColor,
-      this.state.textColor
-    );
     return (
-      <>
-      <div className="column"> <div className="form-container">
-          <h3>Create your t-shirt here!</h3>
-          <form id="generate-words" onSubmit={this.handleSubmit.bind(this)}>
-            <label for="word-count">Word count:</label>
-            <input id="word-count" type="number" />
-            <input type="submit" />
-          </form>
-          <div className="align-text-options">
-            <span>Align Text:</span>
-            <select className="pretty-buttons" onChange={this.changeTextAlign.bind(this)}>
-              <option value="right">Right</option>
-              <option value="center">Center</option>
-              <option value="left">Left</option>
-            </select>
-          </div>
-          <div className="change-font-options">
-            <span>Change font:</span>
-            <select className="pretty-buttons">
-              <option value="right">Comic sans</option>
-              <option value="center">BleepBloop</option>
-            </select>
-          </div>
-          <div className="color-options">
-            <span>Shirt color:</span>
-            {this.colorList.map((color) => {
-              return this.renderColorButton("shirt", color);
-            })}
-          </div>
-          <div className="text-color-options">
-            <span>Text color:</span>
-            {this.colorList.map((color) => {
-              return this.renderColorButton("text", color);
-            })}
-          </div>
-        </div>
-
-        <div className={`tshirt-container shirt-color--white ${shirtClasses}`}>
-          <button className="add-btn" onClick={this.saveShirt.bind(this)}>
-            +
-          </button>
-          <div className="tshirt">
-            <p>{this.state.text}</p>
-          </div>
-        </div></div>
-       
-                 <div className="storage-container">
-          {this.state.savedShirts.map((shirt) => {
-            let newClasses = this.getClasses(
-              shirt.textAlign,
-              shirt.shirtColor,
-              shirt.textColor
-            );
-            return (
-              <div className={`tshirt-container card shirt-color--white ${newClasses}`}>
-                <div className="tshirt">
-                  <p>{shirt.text}</p>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-        </>
+      <Router>
+        <NavLink to="/">Generate!</NavLink>
+        <NavLink to="/saved">Saved Shirts</NavLink>
+        <Switch>
+          <Route path="/saved">
+            <Saved getClasses={this.getClasses} />
+          </Route>
+          <Route path="/">
+            <Generator getClasses={this.getClasses} />
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 }
